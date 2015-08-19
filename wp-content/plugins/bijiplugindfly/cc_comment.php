b9142b900f351e5ade13e45b0e418432c435e5d1
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-19 20:58:16 +0530

    Use existing option and option editing pages in wordpress to show the cccomm editor under general settings tab.

--- wp-content/plugins/bijiplugindfly/cc_comment.php
+++ wp-content/plugins/bijiplugindfly/cc_comment.php
@@ -10,31 +10,29 @@ Author URI: http://www.falkonproductions.com/
 
 function cccomm_init()
 {
-	register_setting('cccomm_options','cccomm_cc_email');
+	register_setting('general','cccomm_cc_email');
 }
 add_action('admin_init','cccomm_init');
 
-function cccomm_option_page()
+function cccomm_setting_field()
 {
 	?>
-	<div class="wrap"><?php screen_icon(); ?>
-	<h2>CC Comments Option Page</h2>
-	<p>Welcome to the CC Comments Plugin. Here you can edit the email(s) you
-	wish to have your comments CC'd to.</p>
-	<form action="options.php" method="post" id="cc-comments-email-options-form">
-	<?php settings_fields('cccomm_options'); ?>
-	<h3><label for="cccomm_cc_email">Email to send CC to: </label> <input
-		type="text" id="cccomm_cc_email" name="cccomm_cc_email"
-		value="<?php echo esc_attr( get_option('cccomm_cc_email') ); ?>" /></h3>
-	<p><input type="submit" name="submit" value="Update Email" /></p>
-	</form>
-	</div>
-	<?php
+	<input type="text" name="cccomm_cc_email" id="cccomm_cc_email"
+			value="<?php echo get_option('cccomm_cc_email'); ?>" />
+	<?php 
+}
+
+function cccomm_setting_section()
+{
+	?>
+	<p>Settings for the CC Comments plugin:</p>
+	<?php 
 }
 
 function cccomm_plugin_menu()
-{
-	add_options_page('CC Comments Settings','CC Comments', 'manage_options', 'cc-comments-plugin', 'cccomm_option_page');
+{ 	
+	add_settings_section('cccomm','CC Comments','cccomm_setting_section','general');
+	add_settings_field('cccomm_cc_email', 'CC Comments Email','cccomm_setting_field','general','cccomm');
 }
 add_action('admin_menu', 'cccomm_plugin_menu');
 
@@ -42,7 +40,7 @@ function cc_comment()
 {
 	global $_REQUEST;
 	
-	$to = "drew@falkonproductions.com";
+	$to = get_option('cccomm_cc_email');
 	$subject = "New comment posted @ yourblog " . $_REQUEST['subject'];
 	$message = "Message from: " . $_REQUEST['name'] . " at email: " . $_REQUEST['email'] . ": \n" . $_REQUEST['comments'];
 	wp_mail($to,$subject,$message);
