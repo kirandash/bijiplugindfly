9dec6cb46fec2f71a005e53740621bb19a5ea36c
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-19 20:29:45 +0530

    Options editing post wordpress. Form submission in new way.

--- wp-content/plugins/bijiplugindfly/cc_comment.php
+++ wp-content/plugins/bijiplugindfly/cc_comment.php
@@ -8,27 +8,25 @@ Version: 1.0
 Author URI: http://www.falkonproductions.com/
  */
 
+function cccomm_init()
+{
+	register_setting('cccomm_options','cccomm_cc_email');
+}
+add_action('admin_init','cccomm_init');
+
 function cccomm_option_page()
 {
-	// first...process form data?
-	if ( check_admin_referer('cccomm_admin_options-update') )
-	{
-		update_option( 'cccomm_cc_email', $_POST['cc_email']);
-		?>
-		<div id="message" class="updated">Email was saved for CC Comments</div>
-		<?php
-	}
 	?>
 	<div class="wrap"><?php screen_icon(); ?>
 	<h2>CC Comments Option Page</h2>
 	<p>Welcome to the CC Comments Plugin. Here you can edit the email(s) you
 	wish to have your comments CC'd to.</p>
-	<form action="" method="post" id="cc-comments-email-options-form">
-	<h3><label for="cc_email">Email to send CC to: </label> <input
-		type="text" id="cc_email" name="cc_email"
+	<form action="options.php" method="post" id="cc-comments-email-options-form">
+	<?php settings_fields('cccomm_options'); ?>
+	<h3><label for="cccomm_cc_email">Email to send CC to: </label> <input
+		type="text" id="cccomm_cc_email" name="cccomm_cc_email"
 		value="<?php echo esc_attr( get_option('cccomm_cc_email') ); ?>" /></h3>
 	<p><input type="submit" name="submit" value="Update Email" /></p>
-	<?php wp_nonce_field('cccomm_admin_options-update'); ?>
 	</form>
 	</div>
 	<?php
@@ -44,7 +42,7 @@ function cc_comment()
 {
 	global $_REQUEST;
 	
-	$to = "drew@somewhere.com";
+	$to = "drew@falkonproductions.com";
 	$subject = "New comment posted @ yourblog " . $_REQUEST['subject'];
 	$message = "Message from: " . $_REQUEST['name'] . " at email: " . $_REQUEST['email'] . ": \n" . $_REQUEST['comments'];
 	wp_mail($to,$subject,$message);
