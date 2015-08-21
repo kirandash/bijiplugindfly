4946ec6c0814ee23e7472b1e133b890ee920e2ea
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-21 15:25:35 +0530

    Solve security issues by using prepare strip tags and wp_kses mentioning allowed html.

--- wp-content/plugins/bijiplugindfly/browser_detector.php
+++ wp-content/plugins/bijiplugindfly/browser_detector.php
@@ -38,7 +38,9 @@ function bdetector_insert_useragent()
 	
 	$table_name = $wpdb->prefix . 'bdetector';
 	
-	$wpdb->insert($table_name,array('user_agent'=>$_SERVER['HTTP_USER_AGENT']),array('%s'));
+	//$wpdb->insert($table_name,array('user_agent'=>$_SERVER['HTTP_USER_AGENT']),array('%s'));
+	$safe_sql = $wpdb->prepare('INSERT INTO ' . $table_name . ' SET user_agent = %1$s',$_SERVER['HTTP_USER_AGENT']);
+	$wpdb->query($safe_sql);
 }
 
 add_action('wp_footer','bdetector_insert_useragent');
--- wp-content/plugins/bijiplugindfly/simpleWidget.php
+++ wp-content/plugins/bijiplugindfly/simpleWidget.php
@@ -2,53 +2,69 @@
 /*
 Plugin Name: Simple Widget
 Plugin URI: http://www.falkonproductions.com/simpleWidget/
-Description: This plugin does things you never thought were possible.
+Description: A simple OOP widget
 Author: Drew Falkman
 Version: 1.0
 Author URI: http://www.falkonproductions.com/
  */
+
 class SimpleWidget extends WP_Widget
 {
-	function SimpleWidget() {
+	function SimpleWidget()
+	{
 		$widget_options = array(
-		'classname'		=>		'simple-widget',
-		'description' 	=>		'Just a simple widget'
-		);
+			'classname'		=>	'simple-widget',
+			'description'	=>	'Just a simple widget');
 		
-		parent::WP_Widget('simple_widget', 'Simple Widget', $widget_options);
+		parent::WP_Widget('simple_widget','Simple Widget', $widget_option);
 	}
 	
-	function widget( $args, $instance ) {
-		extract ( $args, EXTR_SKIP );
+	function widget($args, $instance)
+	{
+		extract( $args, EXTR_SKIP );
 		$title = ( $instance['title'] ) ? $instance['title'] : 'A simple widget';
-		$body = ( $instance['body'] ) ? $instance['body'] : 'A simple message'
+		$body = ( $instance['body'] ) ? $instance['body'] : 'A simple message';
 		?>
-		<?php echo $before_widget ?>
+		<?php echo $before_widget; ?>
 		<?php echo $before_title . $title . $after_title ?>
 		<p><?php echo $body ?></p>
-		<?php 
+		<?php
 	}
 	
-	function form( $instance ) {
+	function update($new_instance,$old_instance)
+	{
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$allowed_html = array('a' => 
+								array('href' => array(),
+								'title' => array()),
+							'br'=> array(),
+							'strong'=> array(),
+							'em'=> array());
+		$instance['body'] = wp_kses($new_instance['body'],$allowed_html);
+		return $instance;
+	}
+	
+	function form( $instance )
+	{
 		?>
-		<label for="<?php echo $this->get_field_id('title'); ?>">
-		Title: 
-		<input id="<?php echo $this->get_field_id('title'); ?>"
-				name="<?php echo $this->get_field_name('title'); ?>"
-				value="<?php echo esc_attr( $instance['title'] ); ?>" />
+		<label for="<?php echo $this->get_field_id('title');?>">
+		Title:
+		<input id="<?php echo $this->get_field_id('title');?>"
+			name="<?php echo $this->get_field_name('title'); ?>"
+			value="<?php echo esc_attr($instance['title']); ?>" />
 		</label>
-		<br />
-		<label for="<?php echo $this->get_field_id('body'); ?>">
-		Message: 
-		<textarea id="<?php echo $this->get_field_id('body'); ?>"
-					name="<?php echo $this->get_field_name('body'); ?>"><?php echo esc_attr( $instance['body'] ); ?></textarea>
+		<label for="<?php echo $this->get_field_id('body');?>">
+		Body:
+		<textarea id="<?php echo $this->get_field_id('body');?>"
+			name="<?php echo $this->get_field_name('body'); ?>"><?php echo esc_attr($instance['body']);?></textarea>
 		</label>
 		<?php 
 	}
-	
 }
-	
-function simple_widget_init() {
+
+function simple_widget_init()
+{
 	register_widget("SimpleWidget");
 }
 add_action('widgets_init','simple_widget_init');
