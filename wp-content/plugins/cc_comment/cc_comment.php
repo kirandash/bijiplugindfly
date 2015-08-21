60082f16c3f0d848da73b58457c9dd243f24d820
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-21 14:41:11 +0530

    Create an uninstall function.

--- wp-content/plugins/cc_comment/cc_comment.php
+++ wp-content/plugins/cc_comment/cc_comment.php
@@ -8,53 +8,17 @@ Version: 1.0
 Author URI: http://www.falkonproductions.com/
  */
 
-function cc_comment()
-{
-	global $_REQUEST;
-	
-	$to = get_option('cccomm_cc_email');
-	$subject = "New comment posted @ yourblog " . $_REQUEST['subject'];
-	$message = "Message from: " . $_REQUEST['name'] . " at email: " . $_REQUEST['email'] . ": \n" . $_REQUEST['comments'];
-	wp_mail($to,$subject,$message);
-}
-
-add_action('comment_post','cc_comment');
-
 function cccomm_init()
 {
 	register_setting('general','cccomm_cc_email');
 }
 add_action('admin_init','cccomm_init');
 
-function cccomm_email_check()
-{
-	$email =isset($_POST['cccomm_cc_email'])?$_POST['cccomm_cc_email']:null;
-	$msg = 'invalid';
-	if ( $email )
-	{
-		if ( is_email($email) )
-		{
-			$msg = 'valid';
-		}
-	}
-	echo $msg;
-	die();
-}
-add_action('wp_ajax_cccomm_email_check','cccomm_email_check');
-add_action('admin_print_scripts-options-general.php', 'cccomm_email_check_script');
-
-function cccomm_email_check_script()
-{
-  wp_enqueue_script( "cc-comments", path_join(WP_PLUGIN_URL, basename( dirname( __FILE__ ) )."/cc_comments.js"), array( 'jquery' ) );
-	
-}
-
 function cccomm_setting_field()
 {
 	?>
 	<input type="text" name="cccomm_cc_email" id="cccomm_cc_email"
-			value="<?php echo get_option('cccomm_cc_email'); ?>" /> 
-	<div id="emailInfo" align="left"></div>
+			value="<?php echo get_option('cccomm_cc_email'); ?>" />
 	<?php 
 }
 
@@ -72,7 +36,17 @@ function cccomm_plugin_menu()
 }
 add_action('admin_menu', 'cccomm_plugin_menu');
 
+function cc_comment()
+{
+	global $_REQUEST;
+	
+	$to = get_option('cccomm_cc_email');
+	$subject = "New comment posted @ yourblog " . $_REQUEST['subject'];
+	$message = "Message from: " . $_REQUEST['name'] . " at email: " . $_REQUEST['email'] . ": \n" . $_REQUEST['comments'];
+	wp_mail($to,$subject,$message);
+}
 
+add_action('comment_post','cc_comment');
 
 register_uninstall_hook(__FILE__,'cccomm_uninstall');
 
--- wp-content/plugins/cc_comment/uninstall.php
+++ wp-content/plugins/cc_comment/uninstall.php
@@ -1,3 +1,6 @@
 <?php
+if ( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+exit();
+
 
 delete_option('cccomm_cc_email');
