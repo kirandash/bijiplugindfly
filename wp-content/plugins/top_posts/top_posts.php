c920db809fdb0e5f34dbcccabad50f7c94f61957
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-21 14:06:01 +0530

    Introduce the loop.Add the top posts widget and plugin.

---
+++ wp-content/plugins/top_posts/top_posts.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Top Posts Plugins
+Plugin URI: http://www.falkonproductions.com/topPosts/
+Description: This plugin will link to the top x posts
+Author: Drew Falkman
+Version: 1.0
+Author URI: http://www.falkonproductions.com/
+ */
+
+function tpp_posts_widget()
+{
+	?>
+	<h3>Posts on this page:</h3>
+	<?php if ( have_posts()) : 
+			while ( have_posts()) : 
+				the_post();
+	?>
+	<div>
+	<a href="<?php echo the_permalink(); ?>"
+		title="<?php echo the_title(); ?>"><?php echo the_title(); ?></a>
+		(<?php echo comments_number(); ?>)
+	</div>
+	<?php 	endwhile; 
+		  endif; 
+	?>
+	<?php 
+}
+
+function tpp_posts_widget_init() {
+	register_sidebar_widget('Top Posts', 'tpp_posts_widget');
+}
+add_action('widgets_init','tpp_posts_widget_init');
